ef8676f4c4899dc2a71a6496ad2b1af6d4c7a615
kaungkaung <kaunghtoo764@gmail.com>
2026-07-06 16:34:52 +0700

    finished to 3.6
    i forgot to push 2.5 :))

---
+++ main.js
@@ -0,0 +1,17 @@
+const { createApp, ref } = Vue
+
+createApp({
+    setup(){
+        const product = ref('Boots')
+        const description = ref('Comfortable and warm')
+        const image = ref('assets/images/socks_blue.jpg')
+        const url = ref('http://www.camt.cmu.ac.th')
+
+        return {
+            product,
+            description,            
+            image,
+            url
+        }
+    }
+}).mount('#app')
