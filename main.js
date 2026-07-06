b83e66cebdc4cec37a1ca94984ed2974beb78bda
kaungkaung <kaunghtoo764@gmail.com>
2026-07-06 17:21:51 +0700

    finished 6.7

--- main.js
+++ main.js
@@ -15,10 +15,20 @@ createApp({
             '20% polyester'
         ])
         const variants = ref([
-            { id: 2234, color: 'green' },
-            { id: 2235, color: 'blue' }
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
         ])
         const sizes = ref(['S', 'M', 'L'])
+        const cart = ref(0)
+        function addToCart() {
+        cart.value += 1
+        }
+        function updateImage(variantImage) {
+            image.value = variantImage
+        }
+        function toggleInStock() {
+            inventory.value = inventory.value > 10 ? 0 : 100
+        }
 
         return {
             product,
@@ -30,7 +40,11 @@ createApp({
             onSale,
             details,
             variants,
-            sizes
+            sizes,
+            cart,
+            addToCart,
+            updateImage,
+            toggleInStock
         }
     }
 }).mount('#app')
