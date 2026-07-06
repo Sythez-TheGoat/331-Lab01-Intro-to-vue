b28b362bc46900d94f4d96daeda276449a3bf80d
kaungkaung <kaunghtoo764@gmail.com>
2026-07-06 16:46:33 +0700

    Finished 4.9

--- main.js
+++ main.js
@@ -6,12 +6,18 @@ createApp({
         const description = ref('Comfortable and warm')
         const image = ref('assets/images/socks_blue.jpg')
         const url = ref('http://www.camt.cmu.ac.th')
+        const inStock = ref(true)
+        const inventory = ref(100)
+        const onSale = ref(true)
 
         return {
             product,
             description,            
             image,
-            url
+            url,
+            inStock,
+            inventory,
+            onSale
         }
     }
 }).mount('#app')
