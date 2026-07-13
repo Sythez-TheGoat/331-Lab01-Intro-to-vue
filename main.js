4077fdc168339d12b23456324085e037300830b6
kaungkaung <kaunghtoo764@gmail.com>
2026-07-13 16:41:06 +0700

    finished 9.10

--- main.js
+++ main.js
@@ -1,66 +1,15 @@
-const { createApp, ref, computed } = Vue
+const { createApp } = Vue
 
-createApp({
-    setup(){
-        const product = ref('Boots')
-        const description = ref('Comfortable and warm')
-        const url = ref('http://www.camt.cmu.ac.th')
-        const inventory = ref(100)
-        const onSale = ref(true)
-        const brand = ref('SE 331')
-        const details = ref([
-            '50% cotton',
-            '30% wool',
-            '20% polyester'
-        ])
-        const variants = ref([
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
-        ])
-        const sizes = ref(['S', 'M', 'L'])
+const app = createApp({
+    setup() {
         const cart = ref(0)
-        const selectedVariant = ref(0)
-
-        const image = computed(() => {
-            return variants.value[selectedVariant.value].image
-        })
-        const inStock = computed(() => {
-            return variants.value[selectedVariant.value].quantity
-        })
-        const title = computed(() => {
-            return brand.value + ' ' + product.value
-        })
-        const onSaleMessage = computed(() => {
-            return brand.value + ' ' + product.value + ' is on sale'
-        })
-
-        function addToCart() {
-            cart.value += 1
-        }
-        function toggleInStock() {
-            inventory.value = inventory.value > 10 ? 0 : 100
-        }
-        function updateVariant(index) {
-            selectedVariant.value = index
-        }
-
+        const premium = ref(true)
         return {
-            description,
-            title,
-            image,
-            url,
-            inStock,
-            inventory,
-            onSale,
-            onSaleMessage,
-            details,
-            variants,
-            sizes,
             cart,
-            selectedVariant,
-            addToCart,
-            toggleInStock,
-            updateVariant
+            premium
         }
     }
-}).mount('#app')
+})
+
+app.component('product-display', productDisplay)
+app.mount('#app')
